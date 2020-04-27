f2e3fbc860e4d9f76621d88659e182acca3efa07
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2020-04-27 23:05:41 +0300

    feat: connect express-handlebars

--- ToDo/index.js
+++ ToDo/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const PORT = process.env.PORT || 3000
 const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('handlebarsEngine', hbs.engine)
+app.set('view engine', 'handlebarsEngine')
+app.set('views', 'views')
 
 async function start() {
   try {
