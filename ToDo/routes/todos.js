da1cf66b161820cc7e93792b788131b0f010975b
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2020-04-28 19:43:08 +0300

    refactor: add second parametr to render method with title value for each page

--- ToDo/routes/todos.js
+++ ToDo/routes/todos.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'Todos list',
+  })
 })
 
 router.get('/create', (req, res) => {
-  res.render('create')
+  res.render('create', {
+    title: 'Create todo',
+  })
 })
 
 module.exports = router
