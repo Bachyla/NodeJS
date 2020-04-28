e85fa99f2a5f0bef086b58b7e86c3c02d6da34e9
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2020-04-28 19:47:23 +0300

    feat: add logic to show active tab

--- ToDo/routes/todos.js
+++ ToDo/routes/todos.js
@@ -4,12 +4,14 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('index', {
     title: 'Todos list',
+    isIndex: true
   })
 })
 
 router.get('/create', (req, res) => {
   res.render('create', {
     title: 'Create todo',
+    isCreate: true
   })
 })
 
